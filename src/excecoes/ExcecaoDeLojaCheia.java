f8da277de0a6dec30416028215754cd7f0b7ff85
lucas <lucaspulsz@gmail.com>
2022-12-01 11:23:43 -0300

    Adding custom exceptions file

---
+++ src/excecoes/ExcecaoDeLojaCheia.java
@@ -0,0 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package excecoes;
+
+public class ExcecaoDeLojaCheia extends Exception {
+    public ExcecaoDeLojaCheia() {}
+
+    public String getMessage() {
+        return "A inclusão não aconteceu porque a loja está cheia!";
+    }
+}
---
+++ src/excecoes/ProdutoNaoEncontrado.java
@@ -0,0 +1,15 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package excecoes;
+
+public class ProdutoNaoEncontrado extends Exception {
+    public ProdutoNaoEncontrado() {}
+
+    public String getMessage() {
+        return "Produto não encontrado";
+    }
+}
+
