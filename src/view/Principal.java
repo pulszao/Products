04c27b6914ff55d1797eb4829d005a394542ca73
lucas <lucaspulsz@gmail.com>
2022-12-02 08:52:23 -0300

    Adding run configuration

---
+++ src/view/Principal.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package view;
+
+import persistencia.PersistenciaComSerializacao;
+
+public class Principal {
+    public Principal() {
+    }
+
+    public static void main(String[] args) {
+        (new Main(new PersistenciaComSerializacao())).menu();
+    }
+}
